c1aa975947f8a8075e65568c81e3cc47df7a0c3f
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2024-04-08 09:29:18 +0200

    Add configuration for Kedro catalog

---
+++ src/reddit_summarizer/datasets/__init__.py
@@ -0,0 +1,3 @@
+from .delta_polars_dataset import DeltaPolarsDataset
+
+__all__ = ["DeltaPolarsDataset"]
--- src/reddit_summarizer/datasets/delta_polars_dataset.py
+++ src/reddit_summarizer/datasets/delta_polars_dataset.py
@@ -1,24 +1,34 @@
 import typing as t
+
 import polars as pl
 from kedro.io import AbstractDataset
 
 
-class DeltaPolarsDataset(AbstractDataset):
+class DeltaPolarsDataset(AbstractDataset[pl.DataFrame, pl.DataFrame]):
+    """``DeltaDataset`` loads/saves data from/to a Delta Table using an underlying
+    filesystem (e.g.: local, S3, GCS). It returns a Polars dataframe.
+    """
+
+    DEFAULT_LOAD_ARGS: dict[str, t.Any] = {}
+    DEFAULT_SAVE_ARGS: dict[str, t.Any] = {}
+
     def __init__(
         self,
-        filepath,
+        filepath: str,
         load_args: dict[str, t.Any] | None = None,
         save_args: dict[str, t.Any] | None = None,
         credentials: dict[str, t.Any] | None = None,
         storage_options: dict[str, t.Any] | None = None,
+        metadata: dict[str, t.Any] | None = None,
     ):
         self._filepath = filepath
-        self._load_args = load_args or {}
-        self._save_args = save_args or {}
+        self._load_args = {**self.DEFAULT_LOAD_ARGS, **(load_args or {})}
+        self._save_args = {**self.DEFAULT_SAVE_ARGS, **(save_args or {})}
         self._credentials = credentials or {}
         self._storage_options = storage_options or {}
 
         self._storage_options.update(self._credentials)
+        self._metadata = metadata or {}
 
     def _load(self) -> pl.DataFrame:
         return pl.read_delta(
@@ -27,9 +37,7 @@ class DeltaPolarsDataset(AbstractDataset):
 
     def _save(self, data: pl.DataFrame) -> None:
         data.write_delta(
-            self._filepath,
-            storage_options=self._storage_options,
-            **self._save_args,
+            self._filepath, storage_options=self._storage_options, **self._save_args
         )
 
     def _describe(self) -> dict[str, t.Any]:
@@ -38,4 +46,5 @@ class DeltaPolarsDataset(AbstractDataset):
             load_args=self._load_args,
             save_args=self._save_args,
             storage_options=self._storage_options,
+            metadata=self._metadata,
         )
